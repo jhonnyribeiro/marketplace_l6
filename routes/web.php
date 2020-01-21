d006ca4345c36de46df76e25ae4a2d40c1ef6d3d
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-21 13:58:35 -0300

    23. Queries no Eloquent

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/model', function (){
-    $products = \App\Product::all(); // select * from products
-
+Route::get('/model', function () {
+//    $products = \App\Product::all(); // select * from products
+//
 //    $user= new \App\User();
-    $user = \App\User::find(1);
-    $user->name = 'Usuario Teste alter...';
-    $user->email = 'email@teste.com';
-    $user->password = bcrypt('12345678');
+//    $user = \App\User::find(1);
+//    $user->name = 'Usuario Teste alter...';
+//    $user->email = 'email@teste.com';
+//    $user->password = bcrypt('12345678');
+//
+//    return $user->save();
+//    $user->save();
 
- //  return $user->save();
-    $user->save();
-    return \App\User::all();
+    // \App\User::all() retorna todos os usuários
+    // \App\User::find(1) retorna usuario especifico
+//    return \App\User::where('name', 'Geo Schuster')->get(); // select * from users where name = 'Geo Schuster'
+    //return \App\User::where('name', 'Geo Schuster')->first(); // select * from users where name = 'Geo Schuster' pega primeir registro encontrado
+
+    return \App\User::paginate(10);
 });
