9e3c6a1da8164f654247c01cb2755122b1489226
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-22 14:17:42 -0300

    Validações produtos - Ajustes Agora testado

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Product;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\ProductRequest;
 class ProductController extends Controller
 {
 
@@ -46,7 +46,7 @@ class ProductController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
         $data = $request->all();
 
@@ -88,7 +88,7 @@ class ProductController extends Controller
      * @param int $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $product)
+    public function update(ProductRequest $request, $product)
     {
         $data = $request->all();
 
