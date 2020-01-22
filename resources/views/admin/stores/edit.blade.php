f82b251627d666ddabd11b36d6fafc608a09e3ef
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-22 11:18:08 -0300

    54. Aplicando Recursos em Lojas

--- resources/views/admin/stores/edit.blade.php
+++ resources/views/admin/stores/edit.blade.php
@@ -4,7 +4,8 @@
     <h1>Editar Loja</h1>
 
     <form action="{{route('admin.stores.update', ['store' => $store->id])}}" method="post">
-        <input type="hidden" name="_token" value="{{csrf_token()}}">
+        @csrf
+        @method("PUT")
         <div class="form-group">
             <label for="name">Loja</label>
             <input type="text" name="name" class="form-control" value="{{$store->name}}">
