f85627c2288892b3740408c09650e157dd9c6d8a
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-22 14:03:52 -0300

    70. Exibindo Validações Views

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -7,22 +7,50 @@
         <input type="hidden" name="_token" value="{{csrf_token()}}">
         <div class="form-group">
             <label for="name">Loja</label>
-            <input type="text" name="name" class="form-control">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+
+            @error('name')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+
         </div>
 
         <div class="form-group">
             <label for="description">Descrição</label>
-            <input type="text" name="description" class="form-control">
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror">
+
+            @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
+
         </div>
 
         <div class="form-group">
             <label for="phone">Telefone</label>
-            <input type="text" name="phone" class="form-control">
+            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror">
+
+            @error('phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
+
         </div>
 
         <div class="form-group">
             <label for="mobile_phone">WhatsApp</label>
-            <input type="text" name="mobile_phone" class="form-control">
+            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror">
+
+            @error('mobile_phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
+
         </div>
 
         <div class="form-group">
