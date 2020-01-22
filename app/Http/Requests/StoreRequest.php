36a7dbec317d02e0f3d1ef09827e1f1df04bc273
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-22 14:10:11 -0300

    72. Traduzindo Mensagens de Validação

--- app/Http/Requests/StoreRequest.php
+++ app/Http/Requests/StoreRequest.php
@@ -30,4 +30,12 @@ class StoreRequest extends FormRequest
             'mobile_phone' => 'required',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'min' => 'Campo deve ter no mínimo :min caracteres'
+        ];
+    }
 }
