69fac6f0eff97aada3c9c57d02b83ed7ed7e24bf
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-23 19:16:43 -0300

    105. Iniciando Adição de Produto no Carrinho

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -32,9 +32,23 @@
                 <span>
                     Loja: {{$product->store->name}}
                 </span>
+
             </div>
+            <div class="product-add col-md-12">
+                <hr>
 
-
+                <form action="{{route('cart.add')}}" method="post">
+                    @csrf
+                    <input type="hidden" name="product[name]" value="{{$product->name}}">
+                    <input type="hidden" name="product[price]" value="{{$product->price}}">
+                    <input type="hidden" name="product[slug]" value="{{$product->slug}}">
+                    <div class="form-group">
+                        <label>Quantidade</label>
+                        <input type="number" name="product[amount]" class="form-control col-md-2" value="1">
+                    </div>
+                    <button class="btn btn-lg btn-danger">Comprar</button>
+                </form>
+            </div>
         </div>
     </div>
 
