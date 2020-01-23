2da667c52f863d6920ac47a3e34bb052674c64f3
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-23 17:38:23 -0300

    102. Produtos na Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Product;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
+    private $prouct;
+
+    public function __construct(Product $product)
+    {
+        $this->prouct = $product;
+    }
     /**
      * Show the application dashboard.
      *
@@ -23,6 +20,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products = $this->prouct->limit(8)->orderBy('id', 'DESC')->get();
+
+        return view('welcome', compact('products'));
     }
 }
