8b77ad4f302699fcd1434ad1ba8cc1f04581cfd6
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-05-10 17:01:19 -0300

    140. Compartilhando Categorias com View Share

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        $categories = \App\Category::all(['name', 'slug']);
+        view()->share('categories', $categories);
     }
 }
