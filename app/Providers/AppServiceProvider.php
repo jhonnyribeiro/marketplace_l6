d9d3ee63592ad363e88b69445b5ff40226f87080
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-05-10 17:17:35 -0300

    141. Usando View Composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        $categories = \App\Category::all(['name', 'slug']);
-        view()->share('categories', $categories);
+
+
     }
 }
